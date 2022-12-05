d8962c855d6ba5be40d2130314a0586608d95480
trevor dewindt <trevor.dewindt@gmail.com>
2022-12-04 18:57:39 -0700

    commit tree 83c649f31dc05e5c2f96c14ec9b0a04f7dfad18c

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(word_arr)
+        initial_word_arr = @word.split('').sort
+        anagrams = []
+        word_arr.each do |e|
+            if e.split('').sort == initial_word_arr
+                anagrams.push(e)
+            end
+        end
+        anagrams
+    end 
+
+end
+
+new_word = Anagram.new("listen")
+new_word.match(%w[enlists google inlets banana lisent])
